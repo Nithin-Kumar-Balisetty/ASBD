601023dd1503a19e8ac4a00ee0a31bbea0fa3da1
Nezuko Agatsuma <nithinkumarbalisetty98@gmail.com>
2022-01-14 09:44:08 +0530

    8 and 9 color added

--- PS-1/Q4.py
+++
@@ -1,8 +0,0 @@
-import statistics
-from scipy.stats import skew
-heights= [167.65, 167, 172, 175, 165, 167, 168, 167, 167.3, 170, 167.5, 170, 167, 169, 172]
-print("Mean height : "+str(statistics.mean(heights)))
-print("Median : "+str(statistics.median(heights)))
-print("Mode : "+str(statistics.mode(heights)))
-print("Standard Deviation : "+str(statistics.pstdev(heights)))
-print("Skewness : "+str(skew(heights)))
--- PS-1/Q6.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-data = {"Studying" : 33,'Sleeping' : 30,'Playing' : 18,'Hobby Activities' : 5}
-data['Spending with Family and Friends'] = 100 - sum(list(data.values()))
-plt.pie(list(data.values()),labels = list(data.keys()),autopct='%1.1f%%')
-plt.title("Pie graph of activities")
-plt.show()
